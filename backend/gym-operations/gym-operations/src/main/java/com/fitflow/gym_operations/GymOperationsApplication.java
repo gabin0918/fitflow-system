9974904f849864b67cee91dcc76564b3d9c32ff8
Gabriela Niedbała <gabi0918n@gmail.com>
2025-12-14 19:17:15 +0100

    Dodanie docker-compose i szkieletu projektu

---
+++ backend/gym-operations/gym-operations/src/main/java/com/fitflow/gym_operations/GymOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.fitflow.gym_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymOperationsApplication.class, args);
+	}
+
+}
---
+++ backend/gym-operations/gym-operations/src/test/java/com/fitflow/gym_operations/GymOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitflow.gym_operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
